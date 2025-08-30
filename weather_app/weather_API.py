6e885d49979cdfa355e45f257a3cfb6e838eb266
Reem kassab <159076075+Reem-Kassab@users.noreply.github.com>
2025-08-31 02:46:38 +0300

    weather_API.py

--- weather_app/weather_API.py
+++ weather_app/weather_API.py
@@ -4,7 +4,11 @@ from utils import kelvin_to_celsius
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+try:
+    from dotenv import load_dotenv
+    load_dotenv()  # load .env only if it exists
+except ModuleNotFoundError:
+    pass  # on Streamlit Cloud, dotenv is not installed
 
 # Get API key from environment variables
 API_KEY = os.getenv("OPENWEATHER_API_KEY") 
@@ -33,3 +37,4 @@ def get_current_weather(city: str) -> dict:
         return result
     else:
         return {"error": f"Failed to fetch data: {response.status_code}"}
+
